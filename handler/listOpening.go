a960bfc485c75fc5965296696a242e1a6c034844
tmazleo <leotomazb@icloud.com>
2025-08-13 15:20:13 -0300

    feat: add crud handler opening

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tmazleo/opportunities/schemas"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error;err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+	sendSuccess(ctx, "list-openings", openings)
 }
