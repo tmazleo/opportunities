a1696a1cc0c44a19ef948bc2c0a4989ee26a85de
tmazleo <leotomazb@icloud.com>
2025-08-14 15:33:42 -0300

    feat: add list documentation

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -7,6 +7,16 @@ import (
 	"github.com/tmazleo/opportunities/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary List opening
+// @Description List all job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningsHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 
